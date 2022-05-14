d8718d8e3ac023e3177bb596700bfee9fad6eac3
Juan Pablo Montoya <juan.montoyao@upb.edu.co>
2022-05-13 22:13:40 -0500

    Reemplazo del welcome y adicion del index

---
+++ proyecto/resources/views/index.blade.php
@@ -0,0 +1,343 @@
+<!--A Design by W3layouts 
+Author: W3layout
+Author URL: http://w3layouts.com
+-->
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>Chur games - Inicio</title>
+	<link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+	<script src="js/jquery.min.js"></script>
+	<!-- Custom Theme files -->
+	<!--theme-style-->
+	<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+	<!--//theme-style-->
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta name="keywords" content="Gaming Creators Responsive web template, Bootstrap Web Templates, Flat Web Templates, Andriod Compatible web template, 
+Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyErricsson, Motorola web design" />
+	<script
+		type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+	<!--fonts-->
+	<link href='//fonts.googleapis.com/css?family=Montserrat+Alternates:400,700' rel='stylesheet' type='text/css'>
+	<link href='//fonts.googleapis.com/css?family=PT+Sans:400,700' rel='stylesheet' type='text/css'>
+	<!--//fonts-->
+	<script src="js/modernizr.custom.js"></script>
+	<link rel="stylesheet" type="text/css" href="css/component.css" />
+</head>
+
+<body>
+	<!--header-->
+	<header>
+		<div class="header">
+
+			<div class="header-top">
+				<div class="container">
+					<div class="head-top">
+						<div class="logo">
+
+							<h1><a href="index.html"><span> C</span>hur <span>G</span>ames</a></h1>
+
+						</div>
+						<div class="top-nav">
+							<span class="menu"><img src="images/menu.png" alt=""> </span>
+
+							<ul>
+								<li class="active"><a class="color1" href="index.html">Inicio</a></li>
+								<li><a class="color2" href="games.html">Juegos</a></li>
+								<li><a class="color3" href="registro.html"  >API</a></li>
+								<li><a class="color5" href="blog.html">Blog</a></li>
+								<li><a class="color6" href="contact.html">Contacto</a></li>
+								<div class="clearfix"> </div>
+							</ul>
+
+							<!--script-->
+							<script>
+								$("span.menu").click(function () {
+									$(".top-nav ul").slideToggle(500, function () {
+									});
+								});
+							</script>
+
+						</div>
+
+						<div class="clearfix"> </div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</header>
+	<!--banner-->
+
+	<div class="banner">
+		<div class="container">
+			<div class="wmuSlider example1">
+				<div class="wmuSliderWrapper">
+					<article style="position: absolute; width: 100%; opacity: 0;">
+						<div class="banner-wrap">
+							<div class="banner-top">
+								<img src="images/12.jpg" class="img-responsive" alt="">
+							</div>
+							<div class="banner-top banner-bottom">
+								<img src="images/11.jpg" class="img-responsive" alt="">
+							</div>
+							<div class="clearfix"> </div>
+						</div>
+
+					</article>
+					<article style="position: absolute; width: 100%; opacity: 0;">
+						<div class="banner-wrap">
+
+							<div class="banner-top">
+								<img src="images/14.jpg" class="img-responsive" alt="">
+							</div>
+							<div class="banner-top banner-bottom">
+								<img src="images/13.jpg" class="img-responsive" alt="">
+							</div>
+							<div class="clearfix"> </div>
+
+						</div>
+					</article>
+					<article style="position: absolute; width: 100%; opacity: 0;">
+						<div class="banner-wrap">
+							<div class="banner-top">
+								<img src="images/16.jpg" class="img-responsive" alt="">
+							</div>
+							<div class="banner-top banner-bottom">
+								<img src="images/15.jpg" class="img-responsive" alt="">
+							</div>
+							<div class="clearfix"> </div>
+						</div>
+					</article>
+				</div>
+				<ul class="wmuSliderPagination">
+					<li><a href="#" class="">0</a></li>
+					<li><a href="#" class="">1</a></li>
+					<li><a href="#" class="wmuActive">2</a></li>
+				</ul>
+			</div>
+			<!---->
+			<script src="js/jquery.wmuSlider.js"></script>
+			<script>
+				$('.example1').wmuSlider({
+					pagination: true,
+					nav: false,
+				});         
+			</script>
+
+		</div>
+	</div>
+	<!--content-->
+	<div class="content">
+		<div class="container">
+			<div class="content-top">
+				<h2 class="new">NUESTRO CATALOGO</h2>
+
+				<div class="wrap">
+					<div class="main">
+						<ul id="og-grid" class="og-grid">
+							<li>
+								<a href="#" data-largesrc="images/1.jpg" data-title="Subway Surfers"
+									data-description="El famoso juego para dispositivos móviles. Sobrevive a los trenes que vienen hacia a tí, evade obstáculos y llega más lejos que los demás! Personaliza tu personaje, mejora tus power-ups y compite con tus amigos para conseguir el mejor tiempo. Tutoriales, guías y consejos disponibles aquí.">
+									<img class="img-responsive" src="images/thumbs/1.jpg" alt="img01" />
+								</a>
+							</li>
+							<li>
+								<a href="#" data-largesrc="images/2.jpg" data-title="Angry Birds"
+									data-description="Sé frío, calculador y no tengas piedad con los cerdos de color verde que están invadiendo el hogar de estos 'inofensivos' pájaros. Catapúltalos hacia la victoria! Supera los niveles, que son cada vez más dificiles y haz uso de aves de distinto tipo con distintas habilidades. Tutoriales, guías y consejos disponibles aquí.">
+									<img class="img-responsive" src="images/thumbs/2.jpg" alt="img02" />
+								</a>
+							</li>
+							<li>
+								<a href="#" data-largesrc="images/3.jpg" data-title="Bike Games"
+									data-description="Si eres aficionado a las motocicletas, este juego es el indicado para tí. Sobrevive a los obstáculos que deja el terreno, salta a través de rampas y evita caerte. Cientos de niveles esperan para que los superes! Guías, tutoriales y consejos aquí.">
+									<img class="img-responsive" src="images/thumbs/3.jpg" alt="img03" />
+								</a>
+							</li>
+							<li>
+								<a href="#" data-largesrc="images/4.jpg" data-title="Temple Run"
+									data-description="Sumérgete en las profundidades de un templo abandonado, sé el cazarrecompensas que siempre soñaste ser y escapa con el tesoro! Este videojuego no te lo puedes perder. Llega cada vez más lejos y supera a tus amigos. Las mejores guías y consejos las podrás encontrar en nuestra página.">
+									<img class="img-responsive" src="images/thumbs/4.jpg" alt="img01" />
+								</a>
+							</li>
+							<li>
+								<a href="#" data-largesrc="images/5.jpg" data-title="Car Games"
+									data-description="Si te gusta la velocidad, los coches y las carreras, debes probar este divertido videojuego en el que medirás tus destrezas de conducción contra jugadores reales al rededor del mundo. Conviertete en el mejor, y personaliza tus coches con las mejores características. Guías, tutoriales y consejos en nuestros blogs.">
+									<img class="img-responsive" src="images/thumbs/5.jpg" alt="img01" />
+								</a>
+							</li>
+							<li>
+								<a href="#" data-largesrc="images/6.jpg" data-title="Fight Games"
+									data-description="Rinde cuentas con tus enemigos en este dinámico y estratégico videojuego al mejor estilo de street-fighter o de Mortal Kombat. Aprendete tus combos, consigue nuevas armas, derrota a tus enemigos y conviertete en el mejor luchador! Guías, tutoriales y consejos en nuestra página web.">
+									<img class="img-responsive" src="images/thumbs/6.jpg" alt="img02" />
+								</a>
+							</li>
+							<li>
+								<a href="#" data-largesrc="images/7.jpg" data-title="Fite Games"
+									data-description="Sé el heroe de esta historia. Estarás ubicado en un mundo donde no eres nadie, debes progresar derrotando monstruos y enemigos para ser la leyenda que pueda rescatar a la princesa al final del juego. Este videojuego es desafiante pero definitivamente divertido y satisfactorio. Encuentra las mejores guías en nuestra página.">
+									<img class="img-responsive" src="images/thumbs/7.jpg" alt="img03" />
+								</a>
+							</li>
+							<li>
+								<a href="#" data-largesrc="images/8.jpg" data-title="Panda Game"
+									data-description="Este videojuego, bastante novedoso y divertido, ha conseguido popularidad en los últimos días. Te mantendremos al tanto de sus actualizaciones y algunas guías para que te familiarices con él. Comparte con otros jugadores en esta enorme comunidad que cada día crece más y más-">
+									<img class="img-responsive" src="images/thumbs/8.jpg" alt="img01" />
+								</a>
+							</li>
+							<div class="clearfix"> </div>
+						</ul>
+					</div>
+				</div>
+			</div>
+			<script src="js/grid.js"></script>
+			<script>
+				$(function () {
+					Grid.init();
+				});
+			</script>
+		</div>
+		<!----->
+		<div class="col-mn">
+			<div class="container">
+				<div class="col-mn2">
+					<h3>Accede a nuestro contenido semanal</h3>
+					<p>Aquí encontrarás diversos artículos relacionados con el universo de los
+						videojuegos móviles, como nuevos lanzamientos, actualizaciones, noticias
+						y más!</p>
+					<a class=" more-in" href="single.html">Leer más</a>
+				</div>
+			</div>
+		</div>
+		<!---->
+		<div class="featured">
+			<div class="container">
+				<div class="col-md-4 latest">
+					<h4>Lo último</h4>
+					<div class="late">
+						<a href="single.html" class="fashion"><img class="img-responsive " src="images/la.jpg"
+								alt=""></a>
+						<div class="grid-product">
+							<span>6 Apr, 2015</span>
+							<p><a href="single.html"> Lanzamiento oficial de un nuevo videojuego de aventura para moviles 
+								</a></p>
+							<a class="comment" href="single.html"><i> </i> 21 Comments</a>
+						</div>
+						<div class="clearfix"> </div>
+					</div>
+					<div class="late">
+						<a href="single.html" class="fashion"><img class="img-responsive " src="images/la1.jpg"
+								alt=""></a>
+						<div class="grid-product">
+							<span>6 Apr, 2015</span>
+							<p><a href="single.html"> Duras críticas al videojuego por actualizaciones innecesarias
+								</a></p>
+							<a class="comment" href="single.html"><i> </i> 0 Comments</a>
+						</div>
+						<div class="clearfix"> </div>
+					</div>
+					<div class="late">
+						<a href="single.html" class="fashion"><img class="img-responsive " src="images/la2.jpg"
+								alt=""></a>
+						<div class="grid-product">
+							<span>6 Apr, 2015</span>
+							<p><a href="single.html"> Cómo superar el tercer nivel de angry birds en dos lanzamientos
+								</a></p>
+							<a class="comment" href="single.html"><i> </i> 0 Comments</a>
+						</div>
+						<div class="clearfix"> </div>
+					</div>
+				</div>
+				<div class="col-md-4 latest">
+					<h4>En tendencia</h4>
+					<div class="late">
+						<a href="single.html" class="fashion"><img class="img-responsive " src="images/la3.jpg"
+								alt=""></a>
+						<div class="grid-product">
+							<span>6 Apr, 2015</span>
+							<p><a href="single.html"> Nuevo record mundial de temple run
+								</a></p>
+							<a class="comment" href="single.html"><i> </i> 0 Comments</a>
+						</div>
+						<div class="clearfix"> </div>
+					</div>
+					<div class="late">
+						<a href="single.html" class="fashion"><img class="img-responsive " src="images/la2.jpg"
+								alt=""></a>
+						<div class="grid-product">
+							<span>6 Apr, 2015</span>
+							<p><a href="single.html"> Cómo superar el tercer nivel de angry birds en dos lanzamientos
+								</a></p>
+							<a class="comment" href="single.html"><i> </i> 0 Comments</a>
+						</div>
+						<div class="clearfix"> </div>
+					</div>
+					<div class="late">
+						<a href="single.html" class="fashion"><img class="img-responsive " src="images/la1.jpg"
+								alt=""></a>
+						<div class="grid-product">
+							<span>6 Apr, 2015</span>
+							<p><a href="single.html"> Duras críticas al videojuego por actualizaciones innecesarias
+								</a></p>
+							<a class="comment" href="single.html"><i> </i> 0 Comments</a>
+						</div>
+						<div class="clearfix"> </div>
+					</div>
+				</div>
+				<div class="col-md-4 latest">
+					<h4>Populares</h4>
+					<div class="late">
+						<a href="single.html" class="fashion"><img class="img-responsive " src="images/la1.jpg"
+								alt=""></a>
+						<div class="grid-product">
+							<span>6 Apr, 2015</span>
+							<p><a href="single.html"> Duras críticas al videojuego por actualizaciones innecesarias
+								</a></p>
+							<a class="comment" href="single.html"><i> </i> 0 Comments</a>
+						</div>
+						<div class="clearfix"> </div>
+					</div>
+					<div class="late">
+						<a href="single.html" class="fashion"><img class="img-responsive " src="images/la.jpg"
+								alt=""></a>
+						<div class="grid-product">
+							<span>6 Apr, 2015</span>
+							<p><a href="single.html"> Lanzamiento oficial de un nuevo videojuego de aventura para moviles 
+								</a></p>
+							<a class="comment" href="single.html"><i> </i> 0 Comments</a>
+						</div>
+						<div class="clearfix"> </div>
+					</div>
+					<div class="late">
+						<a href="single.html" class="fashion"><img class="img-responsive " src="images/la3.jpg"
+								alt=""></a>
+						<div class="grid-product">
+							<span>6 Apr, 2015</span>
+							<p><a href="single.html"> Nuevo record mundial de temple run
+								</a></p>
+							<a class="comment" href="single.html"><i> </i> 0 Comments</a>
+						</div>
+						<div class="clearfix"> </div>
+					</div>
+				</div>
+				<div class="clearfix"> </div>
+			</div>
+		</div>
+	</div>
+	<!---->
+	<div class="footer">
+
+
+		<p class="footer-class">&copy; 2022 Chur Games. Diseñado por <a href="mailto:danielseguro23@gmail.com"
+				target="_blank">Daniel Seguro Rios</a>
+			<a> y </a><a href="mailto:juanpamoro18@gmail.com" target="_blank">Juan Pablo Montoya</a>
+		</p>
+
+	</div>
+	<a href="https://wa.me/+573127024637" target="_blank">
+		<img src="https://icons.iconarchive.com/icons/martz90/circle/48/whatsapp-icon.png" id="btn_whatsapp" alt="">
+	</a>
+	</body>
+
+</html>
