d088bc551f9f0099df47caac99e42db42cd40882
Juan Pablo Montoya <juan.montoyao@upb.edu.co>
2022-05-13 22:27:54 -0500

    agregación de rutas de las vistas

--- proyecto/routes/web.php
+++ proyecto/routes/web.php
@@ -17,6 +17,34 @@ Route::get('/', function () {
     return view('index');
 });
 
+Route::get('/index.html', function () {
+    return view('index');
+});
+
 Route::get('/games.html', function () {
     return view('games');
+});
+
+Route::get('/api.html', function () {
+    return view('api');
+});
+
+Route::get('/blog.html', function () {
+    return view('blog');
+});
+
+Route::get('/contact.html', function () {
+    return view('contact');
+});
+
+Route::get('/logueo.html', function () {
+    return view('logueo');
+});
+
+Route::get('/registro', function () {
+    return view('registro');
+});
+
+Route::get('/single', function () {
+    return view('single');
 });
